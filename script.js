bc40f965d69cc26622ec0c619ca54647d6d16ba7
Muhammadazim <azimjon78@gmail.com>
2021-05-28 14:17:16 +0500

    Landing page

---
+++ script.js
@@ -0,0 +1,31 @@
+var Form1 = document.getElementById("Form1");
+var Form2 = document.getElementById("Form2");
+var Form3 = document.getElementById("Form3");
+
+var Next1 = document.getElementById("Next1");
+var Next2 = document.getElementById("Next2");
+var Back1 = document.getElementById("Back1");
+var Back2 = document.getElementById("Back2");
+
+var progress = document.getElementById("progress");
+
+Next1.onclick = function() {
+    Form1.style.left = "-450px";
+    Form2.style.left = "40px";
+    progress.style.width = "240px";
+}
+Back1.onclick = function() {
+    Form1.style.left = "40px";
+    Form2.style.left = "450px";
+    progress.style.width = "120px";
+}
+Next2.onclick = function() {
+    Form2.style.left = "-450px";
+    Form3.style.left = "40px";
+    progress.style.width = "360px";
+}
+Back2.onclick = function() {
+    Form2.style.left = "40px";
+    Form3.style.left = "450px";
+    progress.style.width = "240px";
+}
